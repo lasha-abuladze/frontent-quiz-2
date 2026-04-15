816de6112f6e504a5600f0f122e265cb1dab1ed2
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-04-15 21:21:06 +0400

    refactor: update dark mode toggle logic for better performance

--- app/script.js
+++ app/script.js
@@ -4,8 +4,6 @@ const btnThemeToggle = document.querySelector(`.btn-theme-toggle`);
 const themeCircle = document.querySelector(`.toggle-circle`);
 const iconToggleArr = document.querySelectorAll(`.icon-toggle`);
 const body = document.querySelector(`body`);
-const subH = document.querySelector(`.sub-h`);
-const quizTopicContainerArr = document.querySelectorAll(`.quiz-topic`);
 
 
 
@@ -13,8 +11,6 @@ const quizTopicContainerArr = document.querySelectorAll(`.quiz-topic`);
 btnThemeToggle.addEventListener(`click`, function() {
     themeCircle.classList.toggle(`toggle-circle--dark-mode`);
     iconToggleArr.forEach(el => el.classList.toggle(`display-none`));
-    body.classList.toggle(`body--dark-mode`);
-    subH.classList.toggle(`sub-h--dark-mode`);
-    quizTopicContainerArr.forEach(el => el.classList.toggle(`quiz-topic--dark-mode`));
+    body.classList.toggle(`style-dark-mode`);
 })
 
